7055bcb392a6c52efc6cc293ab679468fdd30c6c
Nick George <me@izzmo.com>
2023-10-15 13:42:05 -0700

    Update the way we push in a prompt.

--- AIbert.Api/Functions/ChatFunction.cs
+++ AIbert.Api/Functions/ChatFunction.cs
@@ -57,7 +57,7 @@ public class ChatFunction
         var response = req.CreateResponse(HttpStatusCode.OK);
         response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-        response.WriteString(string.Join("\n", _history));
+        await response.WriteStringAsync(string.Join("\n", _history));
 
         return response;
     }
