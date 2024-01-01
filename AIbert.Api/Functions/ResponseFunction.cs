bd778a22ddc6102e5ff85d49ca76863d49b60851
Nick George <me@izzmo.com>
2023-12-31 20:57:20 -0800

    Timer triggers not support.

--- AIbert.Api/Functions/ResponseFunction.cs
+++ AIbert.Api/Functions/ResponseFunction.cs
@@ -2,8 +2,10 @@ using AIbert.Api.Core;
 using AIbert.Api.Services;
 using AIbert.Core;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System.Net;
 using System.Text;
 using System.Text.Json;
 
@@ -27,11 +29,12 @@ namespace AIbert.Api.Functions
         }
 
         [Function("ResponseFunction")]
-        public async Task Run([TimerTrigger("0 */5 * * * *")] Microsoft.Azure.WebJobs.TimerInfo myTimer)
+        //public async Task Run([TimerTrigger("0 */5 * * * *")] Microsoft.Azure.WebJobs.TimerInfo myTimer)
+        public async Task<HttpResponseData> TimerTriggerAsync([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "CheckChats")] HttpRequestData req)
         {
-            if (myTimer.ScheduleStatus is not null)
-            {
-                _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+            //if (myTimer.ScheduleStatus is not null)
+            //{
+                //_logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
 
                 var timeCutoff = DateTimeOffset.UtcNow.AddSeconds(-30);
                 var threads = await _messageHandler.GetAllThreads();
@@ -57,12 +60,15 @@ namespace AIbert.Api.Functions
                                 Content = new StringContent(JsonSerializer.Serialize(body), Encoding.UTF8, "application/json")
                             };
 
-                            using var response = await _client.SendAsync(request);
-                            response.EnsureSuccessStatusCode();
+                            using var res = await _client.SendAsync(request);
+                            res.EnsureSuccessStatusCode();
                         }
                     }
                 }
-            }
+            //}
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            return response;
         }
     }
 }
